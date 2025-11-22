eb3c1362ea3ee335312cf28373716e6806a197eb
Vojin Puric <puric.vojin@gmail.com>
2025-11-22 18:58:06 +0100

    updated go version

--- setup_scripts/install_go.sh
+++ setup_scripts/install_go.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -OL https://go.dev/dl/go1.23.1.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.23.1.linux-amd64.tar.gz
+curl -OL https://go.dev/dl/go1.25.4.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.25.4.linux-amd64.tar.gz
 sudo printf "export PATH=\$PATH:/usr/local/go/bin\n" >> ~/.bashrc
-sudo rm go1.23.1.linux-amd64.tar.gz
+sudo rm go1.25.4.linux-amd64.tar.gz
 git config --global core.editor "nvim -f"
